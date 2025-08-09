5d802b17916786d7036d8ba41369ed5cb35bbcb4
hahnguil <guilherme.f.h@hotmail.com>
2025-08-09 20:13:22 -0300

    feat: implementando loggs na classe UserService para acompanhar melhor o andamento

--- ms-votacao-pauta/src/main/java/br/com/hahn/votacao/domain/service/UserService.java
+++ ms-votacao-pauta/src/main/java/br/com/hahn/votacao/domain/service/UserService.java
@@ -5,11 +5,15 @@ import br.com.hahn.votacao.domain.dto.response.UserResponseDTO;
 import br.com.hahn.votacao.domain.exception.UserAlreadyExistsException;
 import br.com.hahn.votacao.domain.model.User;
 import br.com.hahn.votacao.domain.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 public class UserService {
+
+    private static final Logger userServiceLogger = LoggerFactory.getLogger(UserService.class);
 
     private final UserRepository userRepository;
 
@@ -18,10 +22,14 @@ public class UserService {
     }
 
     public Mono<UserResponseDTO> createUser(UserRequestDTO userRequestDTO) {
+
+        userServiceLogger.info("Criando um novo usuário");
+
         User user = convertToCollection(userRequestDTO);
         return userRepository.existsByuserCPF(userRequestDTO.userCPF())
                 .flatMap(exists -> {
                     if (Boolean.TRUE.equals(exists)) {
+                        userServiceLogger.info("Usuário já existe, exceção sendo lançada");
                         return Mono.error(new UserAlreadyExistsException("User already exists with CPF: " + userRequestDTO.userCPF()));
                     }
                     return userRepository.save(user)
