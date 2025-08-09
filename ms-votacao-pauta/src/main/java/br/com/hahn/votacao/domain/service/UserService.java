e774ff180f4883ba1b686e9520fea63b80aaf230
hahnguil <guilherme.f.h@hotmail.com>
2025-08-09 18:53:17 -0300

    feat: adicionada verificação de existência de usuário para evitar criação de usuários duplicados

--- ms-votacao-pauta/src/main/java/br/com/hahn/votacao/domain/service/UserService.java
+++ ms-votacao-pauta/src/main/java/br/com/hahn/votacao/domain/service/UserService.java
@@ -2,6 +2,7 @@ package br.com.hahn.votacao.domain.service;
 
 import br.com.hahn.votacao.domain.dto.request.UserRequestDTO;
 import br.com.hahn.votacao.domain.dto.response.UserResponseDTO;
+import br.com.hahn.votacao.domain.exception.UserAlreadyExistsException;
 import br.com.hahn.votacao.domain.model.User;
 import br.com.hahn.votacao.domain.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -18,8 +19,14 @@ public class UserService {
 
     public Mono<UserResponseDTO> createUser(UserRequestDTO userRequestDTO) {
         User user = convertToCollection(userRequestDTO);
-        return userRepository.save(user)
-                .map(savedUser -> new UserResponseDTO(savedUser.getUserId(), savedUser.getUserCPF()));
+        return userRepository.existsByuserCPF(userRequestDTO.userCPF())
+                .flatMap(exists -> {
+                    if (Boolean.TRUE.equals(exists)) {
+                        return Mono.error(new UserAlreadyExistsException("User already exists with CPF: " + userRequestDTO.userCPF()));
+                    }
+                    return userRepository.save(user)
+                            .map(savedUser -> new UserResponseDTO(savedUser.getUserId(), savedUser.getUserCPF()));
+                });
     }
 
     private User convertToCollection(UserRequestDTO userRequestDTO) {
