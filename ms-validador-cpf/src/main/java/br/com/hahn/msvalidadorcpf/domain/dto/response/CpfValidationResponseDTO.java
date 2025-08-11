de7e06c7f1c5ea72754d6cae90a413eaa8ffdfe4
hahnguil <guilherme.f.h@hotmail.com>
2025-08-10 22:20:36 -0300

    bug: corrigindo erro no projeto de validação de CFP

--- ms-validador-cpf/src/main/java/br/com/hahn/msvalidadorcpf/domain/dto/response/CpfValidationResponseDTO.java
+++
@@ -1,6 +0,0 @@
-package br.com.hahn.msvalidadorcpf.domain.dto.response;
-
-import br.com.hahn.msvalidadorcpf.domain.enums.CPFStatus;
-
-public record CpfValidationResponseDTO(CPFStatus cpfStatus) {
-}
--- ms-validador-cpf/src/main/java/br/com/hahn/msvalidadorcpf/domain/enums/CPFStatus.java
+++
@@ -1,6 +0,0 @@
-package br.com.hahn.msvalidadorcpf.domain.enums;
-
-public enum CPFStatus {
-    ABLE_TO_VOTE,
-    UNABLE_TO_VOTE
-}
